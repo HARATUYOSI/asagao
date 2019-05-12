b49030d708e49c558b61fba630bb8f50ab7bcd5c
HARATUYOSI <alra9si916323@gmail.com>
2019-05-12 14:07:55 +0000

    メンバーの新規登録、情報編集、削除機能を追加

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -9,23 +9,36 @@ class MembersController < ApplicationController
   end
 
   def new
-
+    @member = Member.new(birthday: Date.new(1997,3,23))
   end
 
   def create
-
+    @member = Member.new(params[:member])
+    if @member.save
+      redirect_to @member, notice: "会員登録しました。"
+    else
+      render 'new'
+    end
   end
 
   def edit
-
+    @member = Member.find(params[:id])
   end
 
   def update
-
+    @member = Member.find(params[:id])
+    @member.assign_attributes(params[:member])
+    if @member.save
+      redirect_to @members, notice: "会員情報を更新しました."
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-
+    @member = Member.find(params[:id])
+    @member.destroy
+    redirect_to :members, notice: "会員を削除しました。"
   end
 
   def show
