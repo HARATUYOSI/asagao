13bdaa3d40648231e20ff34039f8964367aafd50
HARATUYOSI <alra9si916323@gmail.com>
2019-05-14 13:46:12 +0000

    RESTフルなニュース記事リソースを追加

--- db/seeds/development/members.rb
+++ db/seeds/development/members.rb
@@ -14,3 +14,16 @@ gnames = ["太郎","次郎","花子"]
     password_confirmation: 'foobar'
   )
 end
+  0.upto(29) do |idx|
+    Member.create(
+      number: idx + 20,
+      name: "test#{idx +1}",
+      full_name: "test-test#{idx +2}",
+      email: "#{idx * 2}@example.com",
+      birthday: '1997-03-23',
+      sex: 1,
+      administrator: (idx == 0),
+      password: 'foobar',
+      password_confirmation: 'foobar'
+    )
+end
